c1d4e6163d7859e500d20be50ee1b8f6fe2dd32d
Yai <arsenovlidija02@gmail.com>
2021-08-22 16:48:42 +0200

    added updateEvent

---
+++ src/Stores/Events/events-store.js
@@ -0,0 +1,37 @@
+import { writable } from "svelte/store";
+
+const events = writable([]);
+
+const customEventsStore = {
+  subscribe: events.subscribe,
+  addEvent: (eventData) => {
+    const newEvent = {
+      ...eventData,
+      id: Math.random(),
+      isFavorite: false,
+    };
+    events.update((items) => {
+      return [newEvent, ...items];
+    });
+  },
+  toggleFavorite: (id) => {
+    events.update((items) => {
+      const updatedEvent = { ...items.find((e) => e.id === id) };
+      const eventIndex = items.findIndex((e) => e.id === id);
+      updatedEvent.isFavorite = !updatedEvent.isFavorite;
+      const updatedEvents = [...items];
+      updatedEvents[eventIndex] = updatedEvent;
+      return updatedEvents;
+    });
+  },
+  updateEvent: (id, eventData) => {
+    events.update((items) => {
+      const updatedEvent = { ...items.find((e) => e.id === id), ...eventData };
+      const eventIndex = items.findIndex((e) => e.id === id);
+      const updatedEvents = [...items];
+      updatedEvents[eventIndex] = updatedEvent;
+      return updatedEvents;
+    });
+  },
+};
+export default customEventsStore;
