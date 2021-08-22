a89fad467c6414b831f393d9a33474ef57144e3f
Yai <arsenovlidija02@gmail.com>
2021-08-22 16:52:02 +0200

    added editEvent + showDetails functions

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,66 @@
 <script>
+  import events from "./Stores/Events/events-store";
   import Header from "./UI/Header.svelte";
   import EventGrid from "./Components/EventGrid.svelte";
   import EventForm from "./Components/EventForm.svelte";
-  import Modal from "./UI/Modal.svelte";
   import Button from "./UI/Button.svelte";
 
-  let events = [];
-  let opened = false;
+  import EventDetail from "./Components/EventDetail.svelte";
 
-  const submitEventsHandler = (event) => {
-    events = [
-      ...events,
-      {
-        id: Math.random().toString(),
-        title: event.detail.title,
-        subtitle: event.detail.subtitle,
-        address: event.detail.address,
-        image: event.detail.image,
-        email: event.detail.email,
-        description: event.detail.description,
-        isFavorite: event.detail.isFavorite,
-      },
-    ];
+  let opened = false;
+  let eventId = {};
+
+  export let UIMode = "overview";
+
+  const submitEventsHandler = () => {
     opened = false;
+    eventId.id = null;
   };
 
   const toggleFavorite = (event) => {
     const id = event.detail;
-
-    const updatedEvent = { ...events.find((e) => e.id === id) };
-    const eventIndex = events.findIndex((e) => e.id === id);
-    updatedEvent.isFavorite = !updatedEvent.isFavorite;
-    const updatedEvents = [...events];
-    updatedEvents[eventIndex] = updatedEvent;
-    events = updatedEvents;
+    events.toggleFavorite(id);
   };
 
   const cancelEvent = () => {
     opened = false;
+    eventId.id = null;
   };
+
+  const showDetails = (event) => {
+    eventId.id = event.detail;
+    UIMode = "detail";
+  };
+
+  const editEvent = (event) => {
+    opened = true;
+    eventId.id = event.detail;
+  };
+
+  $: console.log(eventId, "id");
 </script>
 
 <Header />
 <main>
   <div class="event-controls">
-    <Button on:click={() => (opened = true)}>New Event</Button>
-    {#if opened}
-      <EventForm
-        on:save-form-data={submitEventsHandler}
-        on:cancel={cancelEvent}
+    {#if UIMode === "overview"}
+      <Button on:click={() => (opened = true)}>New Event</Button>
+      {#if opened}
+        <EventForm
+          on:save-form-data={submitEventsHandler}
+          on:cancel={cancelEvent}
+          id={eventId.id}
+        />
+      {/if}
+      <EventGrid
+        events={$events}
+        on:toggle-favorite={toggleFavorite}
+        on:show-details={showDetails}
+        on:edit-event={editEvent}
       />
+    {:else if UIMode === "detail"}
+      <EventDetail eventId={eventId.id} />
     {/if}
-    <EventGrid {events} on:toggle-favorite={toggleFavorite} />
   </div>
 </main>
 
